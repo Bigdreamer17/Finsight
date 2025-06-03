a4c3c4de3a527c321cbca6b70968d5edfa8fc57e
Joephy527 <106813068+Joephy527@users.noreply.github.com>
2025-06-03 14:22:15 +0300

    update: update landing page

--- web/app/lib/fetchs/get-company.ts
+++ web/app/lib/fetchs/get-company.ts
@@ -78,35 +78,3 @@ export const fetchCompaniesDashboard = async ({
     }
   }
 };
-
-export const fetchCompaniesDashboard = async ({
-  sortParam,
-  sortMetric,
-}: {
-  sortParam: string;
-  sortMetric: string;
-}) => {
-  const session = await auth();
-  const searchParams = new URLSearchParams();
-  searchParams.set("sort_by", sortParam);
-  searchParams.set("order", sortMetric);
-
-  const params = searchParams.toString() ? `?${searchParams.toString()}` : "";
-
-  if (session?.user?.accessToken) {
-    try {
-      const res = await fetch(`${baseUrl}/dashboard/metrics${params}`, {
-        next: { revalidate: 3600 },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-
-      return data;
-    } catch (err) {
-      console.error(err);
-      throw new Error("failed to fetch data");
-    }
-  }
-};
